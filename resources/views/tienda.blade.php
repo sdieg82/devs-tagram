4c468514adfc7e26c149e1a10780655dca1d8d5e
Diego <sdieg82@gmail.com>
2025-10-21 18:27:57 -0500

    web views

---
+++ resources/views/tienda.blade.php
@@ -0,0 +1,11 @@
+ <body>
+        <nav>
+            <ul>
+                <li><a href="/">Principal</a></li>
+                <li><a href="/nosotros">Nosotros</a></li>
+                <li><a href="/tienda">Tienda</a></li>
+            </ul>
+        </nav>
+     <h1>Página principal</h1>
+    </body>
+<h2>Desde el controlador tienda</h2>
